2403b4f0616377dec7c02354466f454ad219e253
joeephwild <josephomotade0@gmail.com>
2022-08-15 16:51:11 +0000

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div style={{ fontFamily: "Libre Bodoni"}} className='flex bg-white p-8 w-full px-4 sticky z-10 justify-between items-center'>
+    <div style={{ fontFamily: "Libre Bodoni"}} className='flex bg-transparent p-8 w-full px-4 sticky z-10 justify-between items-center'>
        <span className='font-bold text-[#b56350] text-xl'>SUJIMOTO</span> 
-       <ul className='flex space-x-6'>
+       <ul className='md:inline-flex hidden space-x-6'>
          <li>Home</li>
          <li>About</li>
          <li>Projects</li>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Hero = () => {
   return (
     <div className=''>
-    <div style={{ fontFamily: "Libre Bodoni"}} className='relative w-full h-[700px]'>
+    <div style={{ fontFamily: "Libre Bodoni"}} className='relative top-0 w-full h-[700px]'>
       <Carousel 
       autoPlay
       infiniteLoop
@@ -22,7 +22,7 @@ const Hero = () => {
         {HeroImage.map((item, id) => {
           return <div className="">
             <img src={item.pics} className="max-w-full h-[700px]" alt={item.name} />
-            <div className='bg-white w-[300px]'>
+            <div className='bg-white object-cover w-[300px]'>
             <div style={{ fontFamily: "Libre Bodoni"}} className='flex flex-col absolute top-[4%] bg-white w-[450px] h-[500x] left-6 justify-center items-center'>
                <span className='text-5xl font-bold'>{item.name}</span>
             </div>
